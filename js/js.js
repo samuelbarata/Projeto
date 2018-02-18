e2f9e37da80349d7def633e16ecc5751e6b862d3
samuelbarata <oliveira3@icloud.com>
2018-02-18 14:34:44 +0000

    admin

--- js/js.js
+++ js/js.js
@@ -1,22 +1,2 @@
 // JavaScript Document
 
-/*NAV*/
-	function openNav() {
-		document.getElementById("mySidenav").style.width = "70%";
-		// document.getElementById("flipkart-navbar").style.width = "50%";
-		document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-	}
-
-	function closeNav() {
-		document.getElementById("mySidenav").style.width = "0";
-		document.body.style.backgroundColor = "rgba(0,0,0,0)";
-	}
-/**/
-
-/*video*/
-
-/**/
-
-/*Carousel*/
-	
-/**/
