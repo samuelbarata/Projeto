e3a5c8af6cb33f654f11864c824b125271374ecb
samuelbarata <oliveira3@icloud.com>
2018-02-18 19:43:32 +0000

    offset for scroll

--- js/js.js
+++ js/js.js
@@ -1,31 +1,29 @@
 // JavaScript Document
+/*
+https://www.w3schools.com/jquery/css_offset.asp
+https://stackoverflow.com/questions/19012495/smooth-scroll-to-div-id-jquery/26129950#26129950
+*/
 
-function scroll() {
+$(document).ready(function(){
 	"use strict";
-    window.scrollBy(0, -150);
-	console.log("scroll");
-}
-
-
-
-
-$("#LinkProjeto").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#proj").offset().top
-    }, 2000);
-});
-$("#LinkCampanha").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#campanha").offset().top
-    }, 2000);
-});
-$("#LinkAdmin").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#Carousel").offset().top
-    }, 2000);
-});
-$("#LinkContactos").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#contactos").offset().top
-    }, 2000);
+	$("#LinkProjeto").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#proj").offset().top-150
+		}, 1500);
+	});
+	$("#LinkCampanha").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#campanha").offset().top-150
+		}, 1500);
+	});
+	$("#LinkAdmin").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#Carousel").offset().top-150
+		}, 1500);
+	});
+	$("#LinkContactos").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#contactos").offset().top-150
+		}, 1500);
+	});
 });
