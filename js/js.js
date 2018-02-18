67c46e805b93bbcf4daacaae8334cb07f8f0afce
samuelbarata <oliveira3@icloud.com>
2018-02-18 15:44:52 +0000

    links

--- js/js.js
+++ js/js.js
@@ -5,3 +5,27 @@ function scroll() {
     window.scrollBy(0, -150);
 	console.log("scroll");
 }
+
+
+
+
+$("#LinkProjeto").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#proj").offset().top
+    }, 2000);
+});
+$("#LinkCampanha").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#campanha").offset().top
+    }, 2000);
+});
+$("#LinkAdmin").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Carousel").offset().top
+    }, 2000);
+});
+$("#LinkContactos").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contactos").offset().top
+    }, 2000);
+});
