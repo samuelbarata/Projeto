e6d9977949fb66540808cef156e2b9fda56a3f9a
samuelbarata <oliveira3@icloud.com>
2018-03-01 13:40:10 +0000

    fixed slider

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,24 @@
 // JavaScript Document
+//https://bootsnipp.com/snippets/featured/colourful-tabbed-slider-carousel
 
 $(document).ready( function() {
+	"use strict";
     $('#myCarousel').carousel({
-		interval:   4000
+    	interval:   4000
 	});
+	
 	var clickEvent = false;
 	$('#myCarousel').on('click', '.nav a', function() {
 			clickEvent = true;
 			$('.nav li').removeClass('active');
 			$(this).parent().addClass('active');		
-	}).on('slid.bs.carousel', function(e) {
+	}).on('slid.bs.carousel', function() {
 		if(!clickEvent) {
 			var count = $('.nav').children().length -1;
 			var current = $('.nav li.active');
 			current.removeClass('active').next().addClass('active');
 			var id = parseInt(current.data('slide-to'));
-			if(count == id) {
+			if(count === id) {
 				$('.nav li').first().addClass('active');	
 			}
 		}
